807433d7ca90dbdefd05f70930dcca773306d0df
slavas <slavas@bk.ru>
2020-09-14 12:37:05 +0300

    set version 0.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shapeutils',
-    version='1.0.0',
+    version='0.0.1',
     author='unknownlighter',
     author_email='unknownlighter@gmail.com',
     packages=find_packages(),
